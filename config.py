0a23ad4ee8ade674ea4c81691cf83542209a3b96
zhangjie <luoyetx@gmail.com>
2018-04-22 09:21:50 +0800

    update model v3

--- config.py
+++ config.py
@@ -50,16 +50,16 @@ cfg.PIXEL_STD = [0.229, 0.224, 0.225]
 cfg.BACKBONE_v2 = {
     'vgg16': (gl.model_zoo.vision.vgg16, 'relu8_fwd', ['conv0', 'conv1', 'conv2', 'conv3']),
     'vgg19': (gl.model_zoo.vision.vgg19, 'relu9_fwd', ['conv0', 'conv1', 'conv2', 'conv3']),
-    'resnet50': (gl.model_zoo.vision.resnet50_v2, 'stage2__plus3', ['conv0']),
+    'resnet50': (gl.model_zoo.vision.resnet50_v1, 'stage2_activation3', ['conv0']),
 }
 
 cfg.BACKBONE_v3 = {
-    'resnet50': (gl.model_zoo.vision.resnet50_v2, ['stage1__plus2', 'stage2__plus3', 'stage3__plus5'], ['conv0']),
+    'resnet50': (gl.model_zoo.vision.resnet50_v1, ['stage1_activation2', 'stage2_activation3', 'stage3_activation5'], ['conv0']),
 }
 
 cfg.BACKBONE_v4 = {
     'vgg19': (gl.model_zoo.vision.vgg19, 'relu9_fwd', ['conv0', 'conv1', 'conv2', 'conv3']),
-    'resnet50': (gl.model_zoo.vision.resnet50_v2, 'stage2__plus3', ['conv0']),
+    'resnet50': (gl.model_zoo.vision.resnet50_v1, 'stage2_activation3', ['conv0']),
 }
 
 cfg.EVAL_NORMAL_IDX = {
--- model.py
+++ model.py
@@ -18,7 +18,7 @@ def freeze_bn(block):
         block.gamma.grad_req = 'null'
         block.beta.grad_req = 'null'
     else:
-        for child in block._children:
+        for child in block._children.values():
             freeze_bn(child)
 
 
@@ -44,6 +44,32 @@ def install_backbone(net, creator, featnames, fixed, pretrained):
         internals = backbone(data).get_internals()
         outs = [internals[out_name] for out_name in out_names]
         net.backbone = gl.SymbolBlock(outs, data, params=backbone.collect_params())
+
+
+##### HardMining on heatmap
+
+class HardMiningFunc(ag.Function):
+
+    def __init__(self, ratio):
+        super(HardMiningFunc, self).__init__()
+        self.ratio = ratio
+
+    def forward(self, x):
+        self.save_for_backward(x)
+        return x.copy()
+
+    def backward(self):
+        x, = self.saved_tensors
+
+
+class HardMiningBlock(gl.HybridBlock):
+
+    def __init__(self, ratio=0.5):
+        super(HardMiningBlock, self).__init__()
+        self.ratio = ratio
+
+    def hybrid_forward(self, F, x):
+        pass
 
 
 ##### model for version 2
@@ -157,78 +183,87 @@ class PoseNet(gl.HybridBlock):
 
 ##### model for version 3
 
+class UpSampling(gl.HybridBlock):
+
+    def __init__(self, num_channel, scale):
+        super(UpSampling, self).__init__()
+        with self.name_scope():
+            self.body = nn.Conv2DTranspose(num_channel, kernel_size=2*scale, strides=scale, padding=scale//2,
+                                           use_bias=False, groups=num_channel, weight_initializer=mx.init.Bilinear())
+            self.body.collect_params().setattr('lr_mult', 0)
+
+    def hybrid_forward(self, F, x):
+        return self.body(x)
+
+
 class CPNGlobalBlock(gl.HybridBlock):
 
-    def __init__(self, num_output, num_channel=64):
+    def __init__(self, num_output, num_channel):
         super(CPNGlobalBlock, self).__init__()
         self.num_output = num_output
         self.num_channel = num_channel
         with self.name_scope():
-            self.P4 = self.block()   # 1/4
-            self.P8 = self.block()   # 1/8
-            self.P16 = self.block()  # 1/16
-            self.T8 = nn.Conv2D(self.num_output, 1, 1, 0)  # 1/16 -> 1/8
-            self.T4 = nn.Conv2D(self.num_output, 1, 1, 0)  # 1/8 -> 1/4
+            self.P4 = nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu')   # 1/4
+            self.P8 = nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu')   # 1/8
+            self.P16 = nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu')  # 1/16
+            self.UpSampling = UpSampling(num_channel, 2)
+            self.T8 = nn.Conv2D(num_channel, 1, 1, 0)  # 1/16 -> 1/8
+            self.T4 = nn.Conv2D(num_channel, 1, 1, 0)  # 1/8 -> 1/4
+            self.Pre4 = nn.HybridSequential()
+            self.Pre4.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
+            self.Pre4.add(nn.Conv2D(num_output, kernel_size=3, padding=1))
+            self.Pre8 = nn.HybridSequential()
+            self.Pre8.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
+            self.Pre8.add(nn.Conv2D(num_output, kernel_size=3, padding=1))
+            self.Pre16 = nn.HybridSequential()
+            self.Pre16.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
+            self.Pre16.add(nn.Conv2D(num_output, kernel_size=3, padding=1))
 
     def hybrid_forward(self, F, x4, x8, x16):
-        F4, F8, F16 = x4, x8, x16
+        F4 = self.P4(x4)
+        F8 = self.P8(x8)
+        F16 = self.P16(x16)
         # 1/16
-        R16 = self.P16(F16)
+        R16 = self.Pre16(F16)
         # 1/8
-        U8 = F.UpSampling(self.T8(R16), scale=2, sample_type='nearest')
-        R8 = self.P8(F8)
-        U8 = F.Crop(U8, R8)
-        R8 = R8 + U8
+        U8 = self.UpSampling(F16)
+        U8 = F.Crop(U8, F8)
+        F8 = F8 + self.T8(U8)
+        R8 = self.Pre8(F8)
         # 1/4
-        U4 = F.UpSampling(self.T4(R8), scale=2, sample_type='nearest')
-        R4 = self.P4(F4)
-        U4 = F.Crop(U4, R4)
-        R4 = R4 + U4
-        return R4, R8, R16
-
-    def block(self):
-        net = nn.HybridSequential()
-        with net.name_scope():
-            net.add(nn.Conv2D(self.num_channel, 3, 1, 1, activation='relu'))
-            net.add(nn.Conv2D(self.num_output, 3, 1, 1))
-        return net
+        U4 = self.UpSampling(F8)
+        U4 = F.Crop(U4, F4)
+        F4 = F4 + self.T4(U4)
+        R4 = self.Pre4(F4)
+        return F4, F8, F16, R4, R8, R16
 
 
 class CPNRefineBlock(gl.HybridBlock):
 
     def __init__(self, num_output, num_channel):
         super(CPNRefineBlock, self).__init__()
-        self.num_output = num_output
-        self.num_channel = num_channel
         with self.name_scope():
-            self.P16 = nn.HybridSequential()
-            self.P16.add(self.bottleneck(), self.bottleneck())
-            self.P8 = nn.HybridSequential()
-            self.P8.add(self.bottleneck())
-            # self.P4 = nn.HybridSequential()
-            # self.P4.add(self.bottleneck(), self.bottleneck(), self.bottleneck())
+            self.P4 = self.block(num_channel)
+            self.P8 = self.block(num_channel)
+            self.P16 = self.block(num_channel)
+            self.U16 = UpSampling(num_channel, 4)
+            self.U8 = UpSampling(num_channel, 2)
             self.R = nn.HybridSequential()
-            self.R.add(self.bottleneck(), nn.Conv2D(self.num_output, 3, 1, 1))
+            self.R.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
+            self.R.add(nn.Conv2D(num_output, kernel_size=3, padding=1))
 
-    def hybrid_forward(self, F, x4, x8, x16):
-        F4, F8, F16 = x4, x8, x16
-        # F4 = self.P4(F4)
-        F8 = self.P8(F8)
-        F16 = self.P16(F16)
-        U16 = F.UpSampling(F16, scale=4, sample_type='nearest')
-        U16 = F.Crop(U16, F4)
-        U8 = F.UpSampling(F8, scale=2, sample_type='nearest')
-        U8 = F.Crop(U8, F4)
-        out = self.R(F.concat(F4, U8, U16))
-        return out
-
-    def bottleneck(self):
+    def block(self, num_channel):
         net = nn.HybridSequential()
-        with net.name_scope():
-            net.add(nn.Conv2D(self.num_channel // 2, 1, 1, 0, activation='relu'))
-            net.add(nn.Conv2D(self.num_channel // 2, 3, 1, 1, activation='relu'))
-            net.add(nn.Conv2D(self.num_channel, 1, 1, 0, activation='relu'))
+        net.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
+        net.add(nn.Conv2D(num_channel, kernel_size=3, padding=1, activation='relu'))
         return net
+
+    def hybrid_forward(self, F, F4, F8, F16):
+        F4 = self.P4(F4)
+        F8 = F.Crop(self.U8(self.P8(F8)), F4)
+        F16 = F.Crop(self.U16(self.P16(F16)), F4)
+        out = self.R(F.concat(F4, F8, F16))
+        return out
 
 
 class CascadePoseNet(gl.HybridBlock):
@@ -244,9 +279,9 @@ class CascadePoseNet(gl.HybridBlock):
 
     def hybrid_forward(self, F, x):
         feats = self.backbone(x)  # pylint: disable=not-callable
-        global_pred = self.global_net(*feats)
-        refine_pred = self.refine_net(*global_pred)
-        return global_pred, refine_pred
+        F4, F8, F16, R4, R8, R16 = self.global_net(*feats)
+        refine_pred = self.refine_net(F4, F8, F16)
+        return [R4, R8, R16], refine_pred
 
     def init_backbone(self, creator, featnames, fixed, pretrained=True):
         assert len(featnames) == 3
