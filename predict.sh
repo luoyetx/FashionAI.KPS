6f80db320e01f2ea85ae2367236f4025cb04b417
zhangjie <luoyetx@gmail.com>
2018-05-31 23:41:40 +0800

    add

--- predict.sh
+++ predict.sh
@@ -1,5 +1,8 @@
-# single v3
-python -u predict.py --model ./output/final/v3/V3.fine.59.scale3.ohkm-resnet50-C256-BS16-adam-0027.params --version 3 --scale 3 --num-worker 3 --gpu 0 --multi-scale --type test --prefix single.v3
+# single v3.1
+python -u predict.py --model ./output/final/v3/V3.fine.59.scale3.ohkm-resnet50-C256-BS16-adam-0027.params --version 3 --scale 3 --num-worker 3 --gpu 0 --multi-scale --type test --prefix single.v3.1
+
+# single v3.2
+python -u predict.py --model ./output/final/v3/V3.fine.59.scale3.ohkm-resnet50-C256-BS16-adam-0013.params --version 3 --scale 3 --num-worker 3 --gpu 0 --multi-scale --type test --prefix single.v3.2
 
 # single v4.1
 python -u predict.py --model ./output/final/v4/V4.noatten.noohkm-resnet50-C256-BS36-adam-0070.params --version 4 --scale 0 --num-worker 3 --gpu 0 --multi-scale --type test --prefix single.v4.1
